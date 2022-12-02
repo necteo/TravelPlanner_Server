2a26936be36e2acf3abbcc86e56a0ca918a2be08
PMO <rhkdah9090@gmail.com>
2022-12-03 02:16:47 +0900

    2022 12 03 0216

--- models/planDetailModel.js
+++ models/planDetailModel.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-
+var detailSchema = new Schema({
+  place_id: { type: Number, required: true },
+  date: { type: Date },
+  startTime: { type: String },
+  endTime: { type: String },
+  details: String,
+});
 const PlanDetailSchema = new Schema({
   plan_id: { type: Number, required: true },
   trip_id: { type: Number, required: true },
   //배열로 해야함
-  plan: {
-    place_id: { type: Number, required: true },
-    date: { type: Date },
-    startTime: { type: String },
-    endTime: { type: String },
-    details: String,
-  },
+  plan: [detailSchema],
 });
+
 const PlanDetail = mongoose.model("Plan_Detail", PlanDetailSchema);
 module.exports = { PlanDetail };
